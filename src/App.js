5f1bf115b7d10db49881a6c76a8160636bb5cf3a
Arif <arif.rasool@aistra.com>
2023-11-27 15:24:54 +0530

    changed the folder structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import './App.css';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import { Paper } from '@mui/material';
-import Mood from './Component/mood';
-import Chat from './Component/chat';
-import Call from './Component/call';
-import Grid from '@mui/material/Grid';
+import React from "react";
+import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { Paper } from "@mui/material";
+import Mood from "./components/mood";
+import Chat from "./components/chat";
+import Call from "./components/call";
+import Grid from "@mui/material/Grid";
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
-      <Container sx={{ padding: '16px' }}>
-      
-        <Box sx={{ bgcolor: '#cfe8fc', height: '90vh', display: 'flex', flexDirection: 'row' , padding: '16px' , gap: 4, margin: '8px 0'}}>
-            <Chat />
-             <Box sx={{  display: 'flex', flexDirection: 'column', gap : 4  }}>
+      <Container sx={{ padding: "16px" }}>
+        <Box
+          sx={{
+            bgcolor: "#cfe8fc",
+            height: "90vh",
+            display: "flex",
+            flexDirection: "row",
+            padding: "16px",
+            gap: 4,
+            margin: "8px 0",
+          }}
+        >
+          <Chat />
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
             <Mood />
             <Call />
-             </Box>
           </Box>
-       
+        </Box>
       </Container>
     </div>
   );
